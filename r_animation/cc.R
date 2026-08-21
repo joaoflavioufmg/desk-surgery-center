baa541120031c2b07004f1b30d321e733a95a0d5
joaoflavio <joao.flavio@dep.ufmg.br>
2026-08-21 16:15:36 -0300

    update R code for resources

--- r_animation/cc.R
+++ r_animation/cc.R
@@ -35,7 +35,14 @@ print(verification_plot)
 # Now create the animation with verified colors
 # NOTE: Replace "hospital_event_log_bupar.csv" with the actual path if running outside a script's directory
 # event_log <- read.csv("C:/Users/user/Desktop/desk/visualization/cc_event_log.csv")
-event_log <- read.csv("cc_event_log.csv")
+event_log <- read.csv("cc_event_log.csv",
+  quote = "\"",
+  stringsAsFactors = FALSE,
+  check.names = FALSE)
+
+
+# Ensure resource is a character field.
+event_log$resource <- trimws(as.character(event_log$resource))
 
 # Map priorities
 if ("priority" %in% names(event_log)) {
